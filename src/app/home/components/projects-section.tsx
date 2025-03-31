94665c4d728c574afcc32c962b94076a2fe40ff9
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2025-03-31 17:34:36 -0600

    changed padding and gaps on mobile

--- src/app/home/components/projects-section.tsx
+++ src/app/home/components/projects-section.tsx
@@ -8,7 +8,7 @@ export default function ProjectsSection({ projects }: ComponentProps) {
   return (
     <div className="text-left w-full px-2">
       <h2 className="text-3xl md:text-5xl font-bold mb-6">My Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-6">
         {/* Project cards will go here */}
         {projects.map((project: Project) => {
           return <ProjectCard key={project.title} project={project} />;
